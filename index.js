8fc54260dcfa64c91a6613904db21de5540f2c29
ZZQ <ZZGaming@list.ru>
2019-10-14 13:51:17 +0800

    add todo list

---
+++ index.js
@@ -0,0 +1,9 @@
+/* TODO:
+[ ] shadowsocks-api.add.server
+[ ] shadowsocks-api.add.port
+---
+[ ] shadowsocks-api.remove.server
+[ ] shadowsocks-api.remove.port
+[ ] shadowsocks-api.get.ping
+[ ] shadowsocks-api.get.allPorts
+*/
