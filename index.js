d882b60d2a77cf721a7f2f92e115a001805db064
ZZQ <ZZGaming@list.ru>
2019-10-18 20:51:15 +0800

    Remove todo list

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-/* TODO:
-[ ] shadowsocks-api.add.server
-[ ] shadowsocks-api.add.port
----
-[ ] shadowsocks-api.remove.server
-[ ] shadowsocks-api.remove.port
-[ ] shadowsocks-api.get.ping
-[ ] shadowsocks-api.get.allPorts
-*/
-
 function SsApi() {
   this.getPing = require('./lib/getPing.js')
+  this.login = require('./lib/login.js')
 }
 
 module.exports = SsApi
