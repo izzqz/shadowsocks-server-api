373533151cd4810b109ae5cd100b3434d2b32149
ZZQ <ZZGaming@list.ru>
2019-10-19 01:11:51 +0800

    Add 'use strict' where it's needed

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+'use strict'
+
 function SsApi() {
-  this.getPing = require('./lib/getPing.js')
   this.login = require('./lib/login.js')
+  this.getPing = require('./lib/getPing.js')
+  this.addPort = require('./lib/addPort.js')
+  this.deletePort = require('./lib/deletePort.js')
 }
 
 module.exports = SsApi
